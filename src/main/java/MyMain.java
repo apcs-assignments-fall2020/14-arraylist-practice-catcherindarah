c81ab4bda48814c884c7032d4d5add8006533387
catcherindarah <22rray@athenian.org>
2020-12-14 23:14:36 +0000

    i completed the assignment

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,27 +1,73 @@
 import java.util.ArrayList;
-
+import java.util.Scanner;
 public class MyMain {
 
     // Counts the number of odd numbers in list
     public static int countOdd(ArrayList<Integer> list) { 
-        // REPLACE WITH YOUR CODE
-        return 0;
+        int x = 0;
+        int y = list.size();
+        for (int i = 0; i < y; i++)
+        {
+            int t = list.get(i);
+            if(t % 2 != 0)
+            {
+                x++;
+            }
+            else
+            {
+                x = x;
+            }
+        }
+        return x;
     }
 
     // Returns true if there is an int that appears in both lists
     public static boolean checkDuplicates(ArrayList<Integer> list1, ArrayList<Integer> list2) { 
-        // REPLACE WITH YOUR CODE
-        return false;
+        int x = 0;
+        int y = list1.size();
+        for(int i = 0; i < y; i++)
+        {
+            int item = list1.get(i);
+            if (list2.contains(item))
+            {
+                x++;
+            }
+            else
+            {
+                x = x;
+            }
+        }
+        if (x!= 0)
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
     }
 
     // Takes an int[] as input and returns the equivalent ArrayList<Integer>
     public static ArrayList<Integer> convertToArrayList(int[] arr) { 
-        // REPLACE WITH YOUR CODE
-        return null;
+        ArrayList<Integer> newint = new ArrayList<Integer>(arr.length);
+        for (int i = 0; i < arr.length; i++)
+        {
+            newint.add(arr[i]);
+        }
+        return newint;
     }
 
 
     public static void main(String[] args) {
-        // Write some code here to test your methods!
+        Scanner scan = new Scanner(System.in);
+        System.out.print("How many numbers do you want to enter?");
+        int n = scan.nextInt();
+        int array[] = new int[n];
+        System.out.println("Enter the " + n + " numbers");
+        for(int i = 0; i < array.length; i++) 
+        {
+            array[i] = scan.nextInt();
+            convertToArrayList(array);
+        }
     }
 }
